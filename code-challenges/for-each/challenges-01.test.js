4136a1dde554674a85ee9162d89a421017567ef2
Davee Sok <daveesok@gmail.com>
2020-06-01 22:35:50 -0700

    fixed trailing spaces

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -40,7 +40,7 @@ The inventory is formatted like this:
   { name: 'bananas', available: true },
   { name: 'blueberries', available: false }
 ]
-This function should use forEach to populate your grocery list based on the store's inventory. 
+This function should use forEach to populate your grocery list based on the store's inventory.
 If the item is available, add it to your list. Return the final list.
 ------------------------------------------------------------------------------------------------ */
 const createList = (availableItems) => {
@@ -59,7 +59,7 @@ Write a function named fizzbuzz that takes in an array of numbers.
 Iterate over the array using forEach to determine the output based on several rules:
   - If a number is divisible by 3, add the word "Fizz" to the output array.
   - If the number is divisible by 5, add the word "Buzz" to the output array.
-  - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz" 
+  - If the number is divisible by both 3 and 5, add the phrase "Fizz Buzz"
   to the output array.
   - Otherwise, add the number to the output array.
 Return the resulting output array.
