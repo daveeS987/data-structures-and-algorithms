763f99316849a48345e3f61c1514e5639a0ee50b
Davee Sok <daveesok@gmail.com>
2020-06-02 17:14:14 -0700

    replaced regular functions with arrow functions

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -4,10 +4,10 @@ CHALLENGE 1 - Review
 Write a function named raisedToTheThird that takes in an array of numbers and returns a new array of
 each of those numbers raised to the 3rd power (hint: look up Math.pow()). Use forEach to solve this problem.
 ------------------------------------------------------------------------------------------------ */
-const raisedToTheThird = (arr) => {
+const raisedToTheThird = arr => {
   // Solution code here...
   let result = [];
-  arr.forEach(function (value) {
+  arr.forEach(value => {
     result.push(Math.pow(value, 3));
   });
   return result;
