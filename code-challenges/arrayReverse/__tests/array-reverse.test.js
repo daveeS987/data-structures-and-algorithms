8aacc9753a98ecfa0b1ccd52517acb52282ee3e9
Davee Sok <daveesok@gmail.com>
2020-08-12 15:44:35 -0700

    completed array reverse

---
+++ code-challenges/arrayReverse/__tests/array-reverse.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let reverse= require('../array-reverse.js');
+
+let array1 = [1, 2, 3, 4, 5, 6];
+reverse(array1);
+
+console.log(array1);
---
+++ code-challenges/arrayReverse/array-reverse.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const reverseArray = (array) => {
+  let start = 0;
+  let end = array.length-1;
+  while(start <= end) {
+    let temp = array[start];
+    array[start] = array[end];
+    array[end] = temp;
+    start+=1;
+    end-=1;
+  }
+};
+
+module.exports = reverseArray;
