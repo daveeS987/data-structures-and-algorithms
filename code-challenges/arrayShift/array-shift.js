1baba87ac0dc69a64120cb10350c11117616c15d
Davee Sok <daveesok@gmail.com>
2020-08-11 18:29:45 -0700

    added test and consoles to test js file

--- code-challenges/arrayShift/array-shift.js
+++ code-challenges/arrayShift/array-shift.js
@@ -1,10 +1,5 @@
 'use strict';
 
-let array1 = [2, 4, 6, 8];
-let target1 = 5;
-
-let array2 = [4, 8, 15, 23, 42];
-let target2 = 16;
 
 function insertShiftArray(arr, target) {
   for (let i = 0; i < arr.length; i++) {
@@ -15,8 +10,4 @@ function insertShiftArray(arr, target) {
   }
 }
 
-insertShiftArray(array1, target1);
-console.log('array1: ', array1);
-
-insertShiftArray(array2, target2);
-console.log('array2: ', array2);
+module.exports = insertShiftArray;
