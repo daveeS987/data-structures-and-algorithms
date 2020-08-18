7ffcaba83e1594fcbb58496e10cc984c0fed2e88
Davee Sok <daveesok@gmail.com>
2020-08-17 23:18:23 -0700

    wrote test

--- code-challenges/arrayBinarySearch/__tests/array-binary-search.test.js
+++ code-challenges/arrayBinarySearch/__tests/array-binary-search.test.js
@@ -1,4 +1,23 @@
-let array = [1, 3, 5, 7, 11, 13, 17, 19];
-let target = 13;
+'use strict';
 
-console.log(binarySearch(array, target));
+let search = require('../array-binary-search.js');
+
+describe('Test binary search', () => {
+
+  it('Proof of Life Test', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('Should return the target index given a target thats present within the array', () => {
+
+    let array = [1, 3, 5, 7, 11, 13, 17, 19];
+    let target = 13;
+    expect(search(array, target)).toStrictEqual(5);
+  });
+
+  it('Should return -1 if target is not within the array', () => {
+    let array = [1,2,3,4,5,6,7,8,9,10];
+    let target = 12;
+    expect(search(array, target)).toStrictEqual(-1);
+  });
+});
