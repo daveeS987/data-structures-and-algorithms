d303c715524a777430aee0c2b3e6f2b15fcdd758
Davee Sok <daveesok@gmail.com>
2020-08-18 14:19:18 -0700

    fixed test on array-shift

--- code-challenges/arrayShift/__tests/array-shift.test.js
+++ code-challenges/arrayShift/__tests/array-shift.test.js
@@ -2,14 +2,24 @@
 
 let insertShiftArray = require('../array-shift.js');
 
-let array1 = [2, 4, 6, 8];
-let target1 = 5;
+describe('Test if element is inserted inside array', () => {
 
-let array2 = [4, 8, 15, 23, 42];
-let target2 = 16;
+  it('Proof of Life Test', () => {
+    expect(true).toBeTruthy();
+  });
 
-insertShiftArray(array1, target1);
-console.log('array1: ', array1);
+  it('Target is inputed into the middle of the array', () => {
+    let array1 = [2, 4, 6, 8];
+    let target1 = 5;
+    insertShiftArray(array1, target1);
+    expect(array1).toStrictEqual([2,4,5,6,8]);
+  });
 
-insertShiftArray(array2, target2);
-console.log('array2: ', array2);
+  it('Target is inputed into the middle of the array', () => {
+    let array2 = [4, 8, 15, 23, 42];
+    let target2 = 16;
+    insertShiftArray(array2, target2);
+    expect(array2).toStrictEqual([4,8,15,16,23,42]);
+  });
+
+});
