f365fc467eb14e44ae9f62fb094c4e5fb357a5f2
Davee Sok <daveesok@gmail.com>
2020-08-24 18:53:10 -0700

    updated with includes and tostring

--- code-challenges/linkedList/__tests/linked-list.test.js
+++ code-challenges/linkedList/__tests/linked-list.test.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const Node = require('../node.js');
+const LinkedList = require('../linked-list.js');
+
+describe('Test if Node works', () => {
+  it('Proof of Life Test', () => {
+    expect(true).toBeTruthy();
+  });
+
+  it('It can instantiate a new node', () => {
+    let node = new Node('should work');
+    let expected = {value: 'should work', next: null};
+    expect(node).toEqual(expected);
+  });
+});
+
+describe('Test if Linked List works', () => {
+  it('Should insantiate linked list', () => {
+    let list = new LinkedList();
+    expect(list).toEqual({head: null});
+  });
+
+  it('should Insert new node at beginning', () => {
+    let list = {
+      head = {
+        value: 'first node',
+        next: null
+      }
+    }
+    list.LinkedList.insert({value: 'new node', next: null})
+  })
+});
