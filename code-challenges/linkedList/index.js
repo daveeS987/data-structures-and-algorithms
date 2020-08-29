51d9125cb5acb1bfb90bb18b18e4275b51127e54
Davee Sok <daveesok@gmail.com>
2020-08-28 22:26:00 -0700

    deleted empty test

--- code-challenges/linkedList/index.js
+++ code-challenges/linkedList/index.js
@@ -8,15 +8,13 @@ list.insert('second');
 list.insert('third');
 list.insert('fourth');
 list.insert('fifth');
+// list.includes('first');
 // list.append('last');
 // list.insertBefore('second', '2.5');
 // list.insertAfter('fourth', '3.5');
+// let k = list.kthFromEnd(2);
+// console.log('using kthfrom end: ', k);
 
 let stringVersion = list.toString();
 console.log('stringVersion: ', stringVersion);
 console.log('list: ', JSON.stringify(list, null, 2));
-// list.includes('first');
-// list.includes('fourth');
-
-// let k = list.kthFromEnd(2);
-// console.log('using kthfrom end: ', k);
