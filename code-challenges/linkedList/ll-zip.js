f46de3c2992e46f968c2cc873b19ec63d3b937ac
Davee Sok <daveesok@gmail.com>
2020-08-31 22:24:16 -0700

    ll-zip works

---
+++ code-challenges/linkedList/ll-zip.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = (list1, list2) => {
+  let l1Temp;
+  let l2Temp;
+  let l1Current = list1.head;
+  let l2Current = list2.head;
+
+  while(l1Current.next) {
+    if(!l2Current.next) {
+      l2Current = l1Current.next;
+      l1Current.next = l2Current;
+      break;
+    }
+
+    l1Temp = l1Current.next;
+    l2Temp = l2Current.next;
+
+    l1Current.next = l2Current;
+    l2Current.next = l1Temp;
+
+    l1Current = l1Temp;
+    l2Current = l2Temp;
+  }
+  if(l2Current) {
+    l1Current.next = l2Current;
+  }
+};
