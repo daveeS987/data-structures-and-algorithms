af10aaf39d9e603c7752884e9c8338a5e93caccf
Davee Sok <daveesok@gmail.com>
2020-10-06 18:05:30 -0700

    wrote skeleton to test

---
+++ javascript/tree/__tests/tree.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const BinarySearchTree = require('../tree.js');
+const Queue = require('../../queues/queues.js');
+
+describe('Proof of life test', () => {
+  it('Proof of life', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+describe('Binary Search Tree should work correctly', () => {
+  it('Can successfully instantiate an empty tree', () => {
+
+  });
+
+  it('Can successfully instantiate a tree with a single root node', () => {
+
+  });
+
+  it('Can successfully add a left child and right child to a single root node', () => {
+
+  });
+
+  it('Can successfully return a collection from a preorder traversal', () => {
+
+  });
+
+  it('Can successfully return a collection from an inorder traversal', () => {
+
+  });
+
+  it('Can successfully return a collection from a postorder traversal', () => {
+
+  });
+});
