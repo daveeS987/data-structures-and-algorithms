2f2833c941cea7884911bd23ac7e6793889a9d70
Davee Sok <daveesok@gmail.com>
2020-11-23 12:42:13 -0800

    still not working, will need to figure out why contains does not properly return true

--- javascript/repeated-word/README.md
+++ javascript/repeated-word/README.md
@@ -13,3 +13,35 @@
 ## Solution
 
 <!-- Embedded whiteboard image -->
+
+Problem Domain:
+
+Given a String, find the first repeating words.
+
+Input: Long String
+Output: the first repeated word or undefined if no words repeat
+
+Considerations:
+
+- What happens with capital vs lowercase?
+- Does the case matter?
+- Need to consider periods, commas, etc that are touching words
+
+Algorithm:
+
+Break down string into an array
+Iterate through the array
+if the current item is in our hashmap
+return that item
+else add to the hashmap
+
+PseudoCode:
+
+declare array = use regex to put all word characters into an array
+declare Hashmap
+
+Iterate through the array
+If (current item is present in our hashmap)
+return current item
+else
+add current item to hashmap
--- javascript/repeated-word/__tests/repeated-word.test.js
+++ javascript/repeated-word/__tests/repeated-word.test.js
@@ -1,7 +1,37 @@
 'use strict';
 
+let Node = require('../repeated-word').Node;
+let LL = require('../repeated-word.js').LL;
+let HashMap = require('../repeated-word.js').HashMap;
+let repeatedWord = require('../repeated-word.js').repeatedWord;
+
 describe('Proof of life', () => {
   it('Proof of life', () => {
     expect(true).toBeTruthy();
   });
+});
+
+describe('Repeated Words function should work correctly', () => {
+
+  it('Example 1', () => {
+    let input = 'Once upon a time, there was a brave princess who...';
+    let actual = repeatedWord(input);
+    let expected = 'a';
+    expect(actual).toEqual(expected);
+  });
+
+  it('Example 2', () => {
+    let input = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...';
+    let actual = repeatedWord(input);
+    let expected = 'it';
+    expect(actual).toEqual(expected);
+  });
+
+  it('Example 3', () => {
+    let input = 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...';
+    let actual = repeatedWord(input);
+    let expected = 'summer';
+    expect(actual).toEqual(expected);
+  });
+
 });
