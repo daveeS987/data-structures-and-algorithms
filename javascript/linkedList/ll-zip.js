92d3db8587eeb82ea82c859965e83c51dd202415
Davee Sok <daveesok@gmail.com>
2020-11-26 19:41:49 -0800

    edited linked list methods

--- javascript/linkedList/ll-zip.js
+++ javascript/linkedList/ll-zip.js
@@ -1,5 +1,31 @@
 'use strict';
 
+// module.exports = (list1, list2) => {
+//   let l1Temp;
+//   let l2Temp;
+//   let l1Current = list1.head;
+//   let l2Current = list2.head;
+
+//   while(l1Current.next) {
+//     if(!l2Current.next) {
+//       l2Current.next = l1Current.next;
+//       l1Current.next = l2Current;
+//       break;
+//     }
+//     l1Temp = l1Current.next;
+//     l2Temp = l2Current.next;
+
+//     l1Current.next = l2Current;
+//     l2Current.next = l1Temp;
+
+//     l1Current = l2Current.next;
+//     l2Current = l2Temp;
+//   }
+//   if(l2Current) {
+//     l1Current.next = l2Current;
+//   }
+// };
+
 module.exports = (list1, list2) => {
   let l1Temp;
   let l2Temp;
@@ -10,17 +36,20 @@ module.exports = (list1, list2) => {
     if(!l2Current.next) {
       l2Current.next = l1Current.next;
       l1Current.next = l2Current;
-      break;
-    }
-    l1Temp = l1Current.next;
-    l2Temp = l2Current.next;
-    l1Current.next = l2Current;
-    l2Current.next = l1Temp;
+      return list1;
+    } else {
+      l1Temp = l1Current.next;
+      l2Temp = l2Current.next;
 
-    l1Current = l2Current.next;
-    l2Current = l2Temp;
+      l1Current.next = l2Current;
+      l2Current.next = l1Temp;
+
+      l1Current = l1Temp;
+      l2Current = l2Temp;
+    }
   }
   if(l2Current) {
     l1Current.next = l2Current;
   }
+  return list1;
 };
