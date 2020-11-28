16fd9c378d1fd2a50163c05c40706e0b14ffc8a9
Davee Sok <daveesok@gmail.com>
2020-11-27 23:09:52 -0800

    finally got fizzbuzz to work

--- javascript/fizzbuzz-tree/README.md
+++ javascript/fizzbuzz-tree/README.md
@@ -6,10 +6,24 @@
 
 <!-- Description of the challenge -->
 
+Given a K-ary tree, create a new tree with the same structure as the original but modify the values according to the following
+
+value divisible by 3 >> replace with "Fizz"  
+value divisible by 5 >> replace with "Buzz"  
+value divisible by 3 & 5 >> replace with "FizzBuzz"  
+if divisible by neither >> turn number into string
+
 ## Approach & Efficiency
 
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
 
+Do a preorder traversal, checking each node value as we traverse, and add to the new tree.
+
+Space: O(n)  
+Time: O(n)
+
 ## Solution
 
 <!-- Embedded whiteboard image -->
+
+![UML](CodeChallenge18-UML.jpg)
---
+++ javascript/fizzbuzz-tree/fizzbuzz-tree.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const TreeNode = require('../tree/tree.js').TreeNode;
+const BinaryTree = require('../tree/tree.js').BinaryTree;
+
+function fizzBuzzTree(tree) {
+
+  let initial = new TreeNode(fzHelper(tree.root.value));
+  let resultTree = new BinaryTree(initial);
+  let newTreeRoot = resultTree.root;
+
+  const walk = (node) => {
+
+    let previous = newTreeRoot;
+
+    if(node.left) {
+      let converted = fzHelper(node.left.value);
+      newTreeRoot.left = new TreeNode(converted);
+      newTreeRoot = newTreeRoot.left;
+      walk(node.left);
+    }
+
+    newTreeRoot = previous;
+
+    if(node.right) {
+      let converted = fzHelper(node.right.value);
+      newTreeRoot.right = new TreeNode(converted);
+      newTreeRoot = newTreeRoot.right;
+      walk(node.right);
+    }
+  };
+
+  walk(tree.root);
+  return resultTree;
+}
+
+
+
+function fzHelper(value) {
+  if((value % 3 === 0) && (value % 5 === 0)) {
+    return 'FizzBuzz';
+  }
+  if((value % 3 === 0) && !(value % 5 === 0)) {
+    return 'Fizz';
+  }
+  if(!(value % 3 === 0) && (value % 5 === 0)) {
+    return 'Buzz';
+  }
+  return value.toString();
+}
+
+module.exports = { fizzBuzzTree, fzHelper};
