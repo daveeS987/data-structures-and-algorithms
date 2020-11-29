75822ec98a5dff67ebe464e66f12806177795f51
Davee Sok <daveesok@gmail.com>
2020-11-28 23:00:28 -0800

    wrote test

--- javascript/left-join/README.md
+++ javascript/left-join/README.md
@@ -1,4 +1,4 @@
-# Hashmap LEFT JOIN
+# Code Challenge 33 : Hashmap LEFT JOIN
 
 <!-- Short summary or background information -->
 
@@ -6,9 +6,16 @@
 
 <!-- Description of the challenge -->
 
+Given two hashmaps, implement a simplified left join. Create a new data structure with all the values of the left hashmap and if the right hashmap also has those values, add to the new data structure.
+
 ## Approach & Efficiency
 
 <!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+Iterate over the first map and add key, value to result object. Object values will be arrays. On each iteration, also check if map2 has the same key. If it does, add it to the corresponding property. if not, add null.
+
+Space: O(n)  
+Time: O(n)
 
 ## Solution
 
---
+++ javascript/left-join/__tests/left-join.test.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const leftJoins = require('../left-join.js');
+
+describe('Proof of life', () => {
+  it('Proof of life', () => {
+    expect(true).toBeTruthy();
+  });
+});
+
+describe('Left Join should work correctly', () => {
+
+  it('It should return correct values', () => {
+    let map1 = new Map();
+    let map2 = new Map();
+
+    map1.set('fond', 'enamored');
+    map1.set('wrath', 'anger');
+    map1.set('diligent', 'employed');
+    map1.set('outfit', 'garb');
+    map1.set('guide', 'usher');
+
+    map2.set('fond', 'averse');
+    map2.set('wrath', 'delight');
+    map2.set('diligent', 'idle');
+    map2.set('guide', 'follow');
+    map2.set('flow', 'jam');
+
+    let expected = [
+      ['fond', 'enamored', 'averse'],
+      ['wrath', 'anger', 'delight'],
+      ['diligent', 'employed', 'idle'],
+      ['outfit', 'garb', null],
+      ['guide', 'usher', 'follow']
+    ];
+
+    let actual = leftJoins(map1, map2);
+    expect(actual).toEqual(expected);
+  });
+
+  it('It should return correct values when no values match', () => {
+
+    let map1 = new Map();
+    let map2 = new Map();
+
+    map1.set('fond', 'enamored');
+    map1.set('wrath', 'anger');
+    map1.set('diligent', 'employed');
+    map1.set('outfit', 'garb');
+    map1.set('guide', 'usher');
+
+    map2.set('fond1', 'averse');
+    map2.set('wrath1', 'delight');
+    map2.set('diligent1', 'idle');
+    map2.set('guide1', 'follow');
+    map2.set('flow1', 'jam');
+
+    let expected = [
+      ['fond', 'enamored', null],
+      ['wrath', 'anger', null],
+      ['diligent', 'employed', null],
+      ['outfit', 'garb', null],
+      ['guide', 'usher', null]
+    ];
+
+    let actual = leftJoins(map1, map2);
+    expect(actual).toEqual(expected);
+  });
+
+  it('It should return correct when left hashmap is smaller', () => {
+
+    let map1 = new Map();
+    let map2 = new Map();
+
+    map1.set('fond', 'enamored');
+    map1.set('wrath', 'anger');
+    map1.set('diligent', 'employed');
+    map1.set('outfit', 'garb');
+    map1.set('guide', 'usher');
+
+    map2.set('fond1', 'averse');
+    map2.set('wrath1', 'delight');
+    map2.set('diligent1', 'idle');
+    map2.set('guide1', 'follow');
+    map2.set('flow1', 'jam');
+    map2.set('diligent3', 'idle');
+    map2.set('guide3', 'follow');
+    map2.set('flow3', 'jam');
+
+    let expected = [
+      ['fond', 'enamored', null],
+      ['wrath', 'anger', null],
+      ['diligent', 'employed', null],
+      ['outfit', 'garb', null],
+      ['guide', 'usher', null]
+    ];
+
+    let actual = leftJoins(map1, map2);
+    expect(actual).toEqual(expected);
+  });
+});
