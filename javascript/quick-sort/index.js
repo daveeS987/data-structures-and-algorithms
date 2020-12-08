99804e919a3b82955ca81107a43b5e60dd7d6ae7
Davee Sok <daveesok@gmail.com>
2020-12-08 14:37:10 -0800

    quicksort still not working

--- javascript/quick-sort/index.js
+++ javascript/quick-sort/index.js
@@ -7,7 +7,7 @@ let arr2 = [20,18,12,8,5,-2];
 let arr3 = [5,12,7,5,5,7];
 let arr4 = [2,3,5,7,13,11];
 
-let result1 = quickSort(arr1);
+let result1 = quickSort(arr1,8,15);
 let result2 = quickSort(arr2);
 let result3 = quickSort(arr3);
 let result4 = quickSort(arr4);
--- javascript/quick-sort/quick-sort.js
+++ javascript/quick-sort/quick-sort.js
@@ -4,9 +4,10 @@ function quickSort(arr, left, right) {
 
   if(left < right) {
     let position = partition(arr, left, right);
-    quickSort(arr, left, position-=1);
-    quickSort(arr, position+=1, right);
+    quickSort(arr, left, position - 1);
+    quickSort(arr, position + 1, right);
   }
+  return arr;
 }
 
 function partition(arr, left, right) {
