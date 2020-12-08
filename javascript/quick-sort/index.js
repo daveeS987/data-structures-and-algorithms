3cf7c726a2414e6acc28c1abb8243f3f9cc04c74
Davee Sok <daveesok@gmail.com>
2020-12-08 15:08:37 -0800

    got quicksort to finally work

--- javascript/quick-sort/index.js
+++ javascript/quick-sort/index.js
@@ -7,11 +7,12 @@ let arr2 = [20,18,12,8,5,-2];
 let arr3 = [5,12,7,5,5,7];
 let arr4 = [2,3,5,7,13,11];
 
-let result1 = quickSort(arr1,8,15);
+quickSort(arr1,8,15);
 let result2 = quickSort(arr2);
 let result3 = quickSort(arr3);
 let result4 = quickSort(arr4);
 
+console.log(({arr1}));
 console.log({result1});
 console.log({result2});
 console.log({result3});
