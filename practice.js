a9d5484a3f3b45fad529a78ebec40237062d7599
Davee Sok <daveesok@gmail.com>
2021-01-12 11:13:29 -0800

    practicing dsa

--- practice.js
+++ practice.js
@@ -126,4 +126,26 @@ class Stack{
     this.top = node;
     return this;
   }
+
+  pop() {
+
+    if(!this.top) {
+      return null;
+    }
+
+    let popped = this.top.value;
+    this.top = this.top.next;
+    return popped;
+  }
+
+  peek() {
+    if(!this.top) {
+      return null;
+    }
+    return this.top.value;
+  }
+
+  isEmpty() {
+    return this.top === null;
+  }
 }
