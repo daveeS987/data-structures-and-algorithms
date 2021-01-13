5c0ac2a66306a383097b15eb454803c26ec0c2ae
Davee Sok <daveesok@gmail.com>
2021-01-12 17:38:06 -0800

    practicing dsa

--- practice.js
+++ practice.js
@@ -149,3 +149,268 @@ class Stack{
     return this.top === null;
   }
 }
+
+class Queue{
+  constructor(node = null) {
+    this.front = node;
+  }
+
+  // enqueue, dequeue, peek, isEmpty
+
+  enqueue(value) {
+
+    let node = new Node(value);
+
+    if(!this.front) {
+      this.front = node;
+      return this;
+    }
+
+    let current = this.front;
+
+    while(current.next) {
+      current = current.next;
+    }
+    current.next = node;
+    return this;
+  }
+
+  dequeue() {
+
+    if(!this.front) {
+      return null;
+    }
+
+    let dequed = this.front.value;
+    this.front = this.front.next;
+    return dequed;
+  }
+
+  peek() {
+
+    if(!this.front) {
+      return null;
+    }
+    return this.front.value;
+  }
+
+  isEmpty() {
+    return this.front === null;
+  }
+}
+
+class TreeNode{
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree{
+  constructor(node = null) {
+    this.root = node;
+  }
+
+  // preOrder, inOrder, postOrder
+
+  // pre: node, left, right
+  preOrder() {
+    let result = [];
+    const walk = node => {
+      result.push(node.value);
+      if(node.left) {walk(node.left);}
+      if(node.right) {walk(node.right);}
+    };
+
+    walk(this.root);
+    return result;
+  }
+
+
+  // inOrder: left, node, right
+  inOrder() {
+    let result = [];
+    const walk = node => {
+      if(node.left) {walk(node.left);}
+      result.push(node.value);
+      if(node.right) {walk(node.right);}
+    };
+    walk(this.root);
+    return result;
+  }
+
+
+  // postOrder: left, right, node
+  postOrder() {
+    let result = [];
+    const walk = node => {
+      if(node.left) {walk(node.left);}
+      if(node.right) {walk(node.right);}
+      result.push(node.value);
+    };
+    walk(this.root);
+    return result;
+  }
+}
+
+class BinarySearchTree extends BinaryTree{
+
+  // addIteravily, addRecursively, containsIteravily, containsRecursively, getMaxIteravily, getMaxRecursively, BreadthFirst
+
+  addIteravily(value) {
+
+    let newNode = new TreeNode(value);
+
+    if(!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let current = this.root;
+    while(current) {
+      if(value > current.value) {
+        if(!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      } else {
+        if(!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      }
+    }
+  }
+
+  addRecursively(value) {
+
+    let newNode = new TreeNode(value);
+
+    if(!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    const walk = node => {
+
+      if(node.value === value) {
+        console.error('This value is already in tree');
+      }
+
+      if(value > node.value) {
+        if(!node.right) {
+          node.right = newNode;
+          return this;
+        }
+        walk(node.right);
+      } else {
+        if(!node.left) {
+          node.left = newNode;
+          return this;
+        }
+        walk(node.left);
+      }
+    };
+
+    walk(this.root);
+    return this;
+  }
+
+  containsIteravily(target) {
+
+    if(!this.root) {
+      return null;
+    }
+
+    let current = this.root;
+    while(current) {
+      if(current.value === target) {
+        return true;
+      }
+
+      if(target > current.value) {
+        current = current.right;
+      } else {
+        current = current.left;
+      }
+    }
+    return false;
+  }
+
+  containsRecursively(target) {
+
+    if(!this.root) {
+      return null;
+    }
+
+    const walk = node => {
+
+      if(node.value === target) {
+        return true;
+      }
+
+      if(!node) {
+        return false;
+      }
+
+      if(target > node.value) {
+        return walk(node.right);
+      } else {
+        return walk(node.left);
+      }
+    };
+
+    return walk(this.root);
+  }
+
+  findMaxIteravily() {
+
+    let current = this.root;
+
+    while(current.right) {
+      current = current.right;
+    }
+
+    return current.value;
+  }
+
+  findMaxRecursively() {
+
+    const walk = node => {
+
+      if(!node.right) {
+        return node.value;
+      }
+
+      return walk(node.right);
+    };
+
+    return walk(this.root);
+  }
+
+  breadthFirst() {
+
+    let result = [];
+    let breadth = new Queue();
+    breadth.enqueue(this.root);
+
+    while(breadth.peek()) {
+
+      let dequed = breadth.dequeue();
+      result.push(dequed.value);
+
+      if(dequed.left) {
+        breadth.enqueue(dequed.left);
+      }
+
+      if(dequed.right) {
+        breadth.enqueue(dequed.right);
+      }
+    }
+
+    return result;
+  }
+}
