244d640a3a568b7587fe0935de20e9959edcc193
Davee Sok <daveesok@gmail.com>
2021-05-27 20:18:23 -0500

    practicing dsa

--- practice.js
+++ practice.js
@@ -356,19 +356,16 @@ class BinarySearchTree extends BinaryTree {
     }
 
     const walk = (node) => {
+      if (!node) {
+        return false;
+      }
       if (node.value === value) {
         return true;
       }
 
       if (node.value > value) {
-        if (!node.right) {
-          return false;
-        }
         return walk(node.right);
       } else {
-        if (!node.left) {
-          return false;
-        }
         return walk(node.left);
       }
     };
