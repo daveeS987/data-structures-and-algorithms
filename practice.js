3b60bf378aea2a3ad283a155111414a15fb6fb44
Davee Sok <daveesok@gmail.com>
2021-06-15 14:22:23 -0500

    practicing dsa

--- practice.js
+++ practice.js
@@ -9,4 +9,102 @@ class LinkedList{
   constructor(node = null){
     this.head = node;
   }
+
+  // insert, append, insertBefore, insertAFter, contains
+  insert(value) {
+
+    let node = new Node(value);
+
+    if(!this.head) {
+      this.head = node;
+      return this
+    }
+
+    node.next = this.head;
+    this.head = node;
+    return this;
+  }
+
+  append(value) {
+
+    let node = new Node(value);
+
+    if(!this.head) {
+      this.head = node;
+      return this.head;
+    }
+
+    let current = this.head;
+
+    while(current.next) {
+      current = current.next;
+    }
+    current.next = node;
+    return this;
+  }
+
+  insertBefore(value, target) {
+
+    let node = new Node(value)
+
+    if(!this.head) {
+      this.head = node;
+      return this;
+    }
+
+    if(this.head.value === target) {
+      node.next = this.head;
+      this.head = node;
+      return this;
+    }
+
+    let current = this.head;
+    while(current.next) {
+      if(current.next.value === target) {
+        node.next = current.next;
+        current.next = node;
+        return this;
+      }
+      current = current.next;
+    }
+    console.error('Target is not within list')
+  }
+
+  insertAfter(value, target) {
+
+    let node = new Node(value);
+
+    if(!this.head) {
+      this.head = node;
+      return this;
+    }
+
+    let current = this.head;
+
+    while(current) {
+      if(current.value === target) {
+        node.next = current.next;
+        current.next = node;
+        return this;
+      }
+      current = current.next;
+    }
+    console.error('Target not within list')
+  }
+
+  contains(target) {
+
+    if(!this.head) {
+      return null;
+    }
+
+    let current = this.head;
+    while(current) {
+      if(current.value === target) {
+        return true
+      }
+      current = current.next
+    }
+    return false;
+  }
 }
