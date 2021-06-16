c66bbac5a00e22d3a1f8b061dc58a703d59cc433
Davee Sok <daveesok@gmail.com>
2021-06-16 16:01:37 -0500

    practicing dsa

--- practice.js
+++ practice.js
@@ -108,3 +108,91 @@ class LinkedList{
     return false;
   }
 }
+
+class Stack{
+  constructor(node = null){
+    this.top = node;
+  }
+
+  // push, pop, peek, isEmpty
+  push(value) {
+
+    let node = new Node(value);
+    node.next = this.front;
+    this.front = node;
+    return this;
+  }
+
+  pop() {
+
+    if(!this.top) {
+      return null;
+    }
+
+    let popped = this.top.value;
+    this.top = this.top.next;
+    return popped;
+  }
+
+  peek() {
+
+    if(!this.top) {
+      return null;
+    }
+
+    return this.top.value;
+  }
+
+  isEmpty() {
+
+    return this.top === null;
+  }
+}
+
+class Queue{
+  constructor(node = null) {
+    this.front = node;
+  }
+
+  // enqueue, dequeue, peek, isEmpty
+
+  enqueue(value) {
+
+    let node = new Node(value);
+
+    if(!this.front) {
+      this.front = node;
+      return this;
+    }
+
+    let current = this.head;
+    while(current.next) {
+      current = current.next;
+    }
+    current.next = node;
+    return this;
+  }
+
+  dequeue() {
+
+    if(!this.front) {
+      return null;
+    }
+
+    let dequeud = this.front.value;
+    this.front = this.front.next;
+    return dequeud;
+  }
+
+  peek() {
+
+    if(!this.front) {
+      return null;
+    }
+    return this.front.value;
+  }
+
+  isEmpty() {
+    return this.front === null;
+  }
+}
