8a428c24bc77f090cd99063582e068c2676b8272
Davee Sok <daveesok@gmail.com>
2021-07-13 00:15:58 -0500

    finished code and test

--- python/animal_shelter/animal_shelter.py
+++ python/animal_shelter/animal_shelter.py
@@ -1,7 +1,18 @@
+import copy
+
+
 class Node:
     def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
+
+
+"""
+animal object
+{
+    animal: "dog"
+}
+"""
 
 
 class Animal_Shelter:
@@ -11,7 +22,53 @@ class Animal_Shelter:
         self.length = 0
 
     def enqueue(self, obj):
-        pass
+
+        node = Node(obj)
+
+        if self.front is None:
+            self.front = node
+            self.rear = node
+            return self
+
+        self.rear.next = node
+        self.rear = node
+        self.length += 1
+        return self
 
     def dequeue(self, pref):
-        pass
+
+        if self.front is None:
+            raise Exception("Queue is Empty")
+
+        if self.front.value["animal"] == pref:
+            dequed = self.front.value["animal"]
+            self.front = self.front.next
+            self.length -= 1
+            return dequed
+
+        temp_length = copy.deepcopy(self.length)
+        answer = None
+
+        while temp_length >= 0:
+            if self.front.value["animal"] == pref:
+                answer = self.front.value["animal"]
+                self.front = self.front.next
+                self.length -= 1
+                temp_length -= 1
+                break
+            else:
+                dequed = self.front.value
+                dequed_node = Node(dequed)
+                self.front = self.front.next
+                self.rear.next = dequed_node
+                self.rear = dequed_node
+                temp_length -= 1
+
+        for i in range(temp_length + 1):
+            dequed = self.front.value
+            dequed_node = Node(dequed)
+            self.front = self.front.next
+            self.rear.next = dequed_node
+            self.rear = dequed_node
+
+        return answer
--- python/tests/test_animal_shelter.py
+++ python/tests/test_animal_shelter.py
@@ -8,12 +8,46 @@ def test_node_class_can_instantiate():
 
 
 def test_can_Animal_shelter_instantiate():
-    pass
+    new_shelter = Animal_Shelter()
+    assert new_shelter.front == None
+    assert new_shelter.rear == None
+    assert new_shelter.length == 0
 
 
 def test_can_enqueue_animal_object_into_Animal_shelter():
-    pass
+    new_shelter = Animal_Shelter()
+    input1 = {"animal": "dog"}
+    new_shelter.enqueue(input1)
+    assert new_shelter.front.value["animal"] == "dog"
+    assert new_shelter.rear.value["animal"] == "dog"
 
 
 def test_can_dequeue_animal_from_Animal_shelter():
-    pass
+    new_shelter = Animal_Shelter()
+    input1 = {"animal": "dog"}
+    input2 = {"animal": "cat"}
+    new_shelter.enqueue(input1)
+    new_shelter.enqueue(input2)
+    actual = new_shelter.dequeue("dog")
+    expected = "dog"
+    assert actual == expected
+    assert new_shelter.front.value["animal"] == "cat"
+
+
+def test_can_dequeue_animal_from_Animal_shelter():
+    new_shelter = Animal_Shelter()
+    input1 = {"animal": "dog"}
+    input2 = {"animal": "dog"}
+    input3 = {"animal": "cat"}
+    input4 = {"animal": "dog"}
+    input5 = {"animal": "cat"}
+    new_shelter.enqueue(input1)
+    new_shelter.enqueue(input2)
+    new_shelter.enqueue(input3)
+    new_shelter.enqueue(input4)
+    new_shelter.enqueue(input5)
+    actual = new_shelter.dequeue("cat")
+    expected = "cat"
+    assert actual == expected
+    assert new_shelter.front.value["animal"] == "dog"
+    assert new_shelter.rear.value["animal"] == "cat"
