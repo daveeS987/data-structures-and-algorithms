59442db5b4f58bd1a0a70309b9652833384811b9
Davee Sok <daveesok@gmail.com>
2021-07-12 23:30:56 -0500

    forgot to check into branch.  committing for now

---
+++ python/animal_shelter/animal_shelter.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value=None, next=None):
+        self.value = value
+        self.next = next
+
+
+class Animal_Shelter:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+        self.length = 0
+
+    def enqueue(self, obj):
+        pass
+
+    def dequeue(self, pref):
+        pass
---
+++ python/tests/test_animal_shelter.py
@@ -0,0 +1,19 @@
+import pytest
+
+from animal_shelter.animal_shelter import Animal_Shelter, Node
+
+
+def test_node_class_can_instantiate():
+    pass
+
+
+def test_can_Animal_shelter_instantiate():
+    pass
+
+
+def test_can_enqueue_animal_object_into_Animal_shelter():
+    pass
+
+
+def test_can_dequeue_animal_from_Animal_shelter():
+    pass
