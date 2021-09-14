e9727be0d8791d83ce5d583fc37b058ad1a1f840
Davee Sok <daveesok@gmail.com>
2021-09-14 11:06:07 -0500

    edited test

--- python/tests/test_left_join.py
+++ python/tests/test_left_join.py
@@ -3,14 +3,16 @@ import pytest
 from left_join.left_join import left_join
 
 
-def test_function_will_account_for_empty_left(hashmap1):
-    actual = left_join({}, hashmap1)
+def test_function_will_account_for_empty_left():
+    obj1 = {"a": "aa", "b": "bb", "c": "cc", "d": "dd", "e": "ee"}
+    actual = left_join({}, obj1)
     expected = []
     assert actual == expected
 
 
-def test_function_will_account_for_empty_right(hashmap1):
-    actual = left_join(hashmap1, {})
+def test_function_will_account_for_empty_right():
+    obj1 = {"a": "aa", "b": "bb", "c": "cc", "d": "dd", "e": "ee"}
+    actual = left_join(obj1, {})
     expected = [["a", "aa", None], ["b", "bb", None], ["c", "cc", None], ["d", "dd", None], ["e", "ee", None]]
     assert actual == expected
 
@@ -44,20 +46,3 @@ def test_function_will_account_for_right_side_larger():
     actual = left_join(obj1, obj2)
     expected = [["c", "cc", "cc"], ["v", "vv", None], ["e", "ee", "ee"]]
     assert actual == expected
-
-
-######################
-# Fixtures
-######################
-
-
-@pytest.fixture
-def hashmap1():
-    obj = {"a": "aa", "b": "bb", "c": "cc", "d": "dd", "e": "ee"}
-    return obj
-
-
-@pytest.fixture
-def hashmap2():
-    obj = {"a": "aa", "b": "bb", "g": "gg", "h": "hh", "e": "ee"}
-    return obj
