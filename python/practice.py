136b0928b497796b9317a111c688fb1634329133
Davee Sok <daveesok@gmail.com>
2022-02-14 19:54:16 -0600

    updated package json

---
+++ python/practice.py
@@ -0,0 +1,87 @@
+# node
+"""
+value
+
+next
+
+"""
+
+
+class Node:
+    def __init__(self, value=None, node=None):
+        self.value = value
+        self.next = node
+
+
+# linkedlist
+"""
+append
+
+insert
+
+insertBefore
+
+insertAfter
+
+remove
+
+contains
+"""
+
+
+class LinkedList:
+    def __init__(self, node=None):
+        self.head = node
+
+
+# stacks
+"""
+pop
+
+push
+
+peek
+
+isEmpty
+
+"""
+
+# queues
+"""
+enqueue
+
+dequeue
+
+isEmpty
+
+peek
+
+"""
+
+# trees
+"""
+inOrder
+
+
+preOrder
+
+
+postOrder
+
+
+append
+
+
+contains
+
+
+breadthFirst
+
+
+"""
+
+
+# hashmaps
+"""
+ha
+"""
